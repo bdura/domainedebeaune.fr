2497f08bf1bd7e9cb2eb043da5bff39fbfd9879f
Basile Dura <basile@bdura.me>
2024-02-11 12:02:25 +0100

    fix: prose elements

---
+++ components/content/ProseA.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps({
+  href: {
+    type: String,
+    default: "",
+  },
+  target: {
+    type: String,
+    default: undefined,
+    required: false,
+  },
+});
+</script>
+
+<template>
+  <NuxtLink
+    :href="href"
+    target="_blank"
+    class="text-secondary hover:text-secondary/80 cursor-pointer"
+  >
+    <slot></slot>
+  </NuxtLink>
+</template>
---
+++ components/content/ProseUl.vue
@@ -0,0 +1,5 @@
+<template>
+  <ul class="list-disc list-inside mb-3 pl-2">
+    <slot></slot>
+  </ul>
+</template>
