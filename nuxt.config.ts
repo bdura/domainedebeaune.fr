2a2679c87cb7f86d6e2ac45e65fbc0c7310c4f59
Basile Dura <basile@bdura.me>
2024-02-11 09:52:00 +0100

    feat: change logo & description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Domaine de Beaune",
+      link: [{ rel: "icon", type: "image/x-icon", href: "/apple.svg" }],
+      meta: [
+        { property: "og:title", content: "Domaine de Beaune" },
+        {
+          property: "og:description",
+          content:
+            "Nous sommes une exploitation agricole familiale et produisons entre autres des pommes sur 4 hectares. Grâce à l'eau du ciel, le soleil, le sol riche et volcanique des puys et un usage minimaliste et raisonné de traitements, les fruits profitent du terroir exceptionnel des coteaux d'Auvergne pour murir à leur rythme.",
+        },
+      ],
+    },
+  },
+  content: {
+    contentHead: false,
+  },
   devtools: { enabled: true },
   modules: [
     "@nuxtjs/tailwindcss",
